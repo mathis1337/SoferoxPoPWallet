a1a5c8e609acea2d7625e94667e00adaec8cfd42
mathis1337 <aaron.mathis13@gmail.com>
2019-08-12 19:35:07 -0500

    client data methods

---
+++ Soferox/Data/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Soferox
+{
+    class Client
+    {
+        protected readonly string FirstName, LastName, Age ,Email, Country, Password,PhonNumber;
+        protected decimal Balance = 0.00000000M;
+
+        public Client(string Email,string Password, string FirstName, string LastName, string Country,
+                      string Age, string PhonNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Country = Country;
+            this.Age = Age;
+            this.Password = Password;
+            this.PhonNumber = PhonNumber;
+            Balance = 0.00000000M;
+        }
+        public Client(string Email,string Password,string FirstName, string LastName, string Country,
+                      string Age, string PhonNumber,decimal Balance)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Country = Country;
+            this.Age = Age;
+            this.Password = Password;
+            this.PhonNumber = PhonNumber;
+            this.Balance = Balance;
+        }
+        public string FName
+        {
+            get { return FirstName; }
+        }
+        public string LName
+        {
+            get { return LastName; }
+        }
+        public string EM
+        {
+            get { return Email; }
+        }
+        public string Ctry
+        {
+            get { return Country; }
+        }
+        public string Ag
+        {
+            get { return Age; }
+        }
+        public string Pass
+        {
+            get { return Password; }
+        }
+        public string Phone
+        {
+            get { return PhonNumber; }
+        }
+
+        public decimal Bal
+        {
+            set { Balance = value; }
+            get { return Balance; }
+        }
+    }
+}
---
+++ Soferox/Data/ClientsLogs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Soferox
+{
+    class ClientsLogs
+    {
+        static FileStream fs;
+        static BinaryReader br;
+        static BinaryWriter bw;
+        public static string OpenStreams()
+        {
+            try
+            {
+                //if you get an error about this please add this file into you bin/debug folder
+                fs = new FileStream("Logs.txt", FileMode.Open, FileAccess.ReadWrite);
+                br = new BinaryReader(fs);
+                bw = new BinaryWriter(fs);
+                return "Opened";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public static void SetAdress(int Address)
+        {
+            fs.Seek(2 * Address, SeekOrigin.Begin);
+        }
+        public static void GetLogs(DataGridView dg)
+        {
+            while (br.PeekChar() == '|')
+            {
+                br.ReadChar();
+                dg.Rows.Add(br.ReadString(), br.ReadString());
+            }
+        }
+        public static void AddLog(string s1,string s2)
+        {
+            bw.Write('|');
+            bw.Write(s1);
+            bw.Write(s2);
+            bw.Flush();
+        }
+        public static bool CloseStreams()
+        {
+            try
+            {
+                br.Close();
+                bw.Close();
+                fs.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
